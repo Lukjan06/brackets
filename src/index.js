a011db9aade883ea7142850a1609b805af463569
Anastasya <elka-mogilev@mail.ru>
2022-02-22 14:35:55 +0300

    feat: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = 
+function check(str, bracketsConfig) {
   // your solution
-}
+
+  let arr = Array.from(str);
+    let result = [];
+
+
+    for( let i = 0; i < arr.length; i++) {
+      if(arr[i] === '(') {
+        result.push('(')
+      } else if (arr[i] === ')'&& result.length > 0 &&  result[result.length-1] === '(') {
+        result.pop();
+      } else if (arr[i] === '[') {
+        result.push('[')
+      } else if (arr[i] === ']' && result.length > 0 &&  result[result.length-1] === '[') {
+        result.pop();
+      } else if (arr[i] === '{') {
+        result.push('{')
+      } else if (arr[i] === '}' && result.length > 0 &&  result[result.length-1] === '{') {
+        result.pop();
+      } else if (arr[i] === '|' && !result.includes(arr[i]) || result.length == 0) {
+        result.push('|')
+      } else if(arr[i] === '|' && result.length > 0 &&  result[result.length-1] === '|') {
+        result.pop();}
+      }
+ 
+      
+      
+      if(result.length === 0 ) {
+        return true
+      } else {  return false
+       }
+
+
+    }
